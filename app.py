be2d491e303efdd95ba0d2de043d9ee041785bd1
Al-Hasib <mdabdullahalhasib1111@gmail.com>
2024-05-06 18:16:15 +0600

    transformer added to streamlit

--- app.py
+++ app.py
@@ -5,22 +5,26 @@ from utils import *
 
 def textblobSentiment(user_input):
     textblob_sentiment = textblob_score(user_input)
-    textblob_str  = (f"**TextBlob Score :** {textblob_sentiment}")
+    textblob_str  = (f"**TextBlob :**      {textblob_sentiment}")
     st.write(textblob_str)
 
 def VaderSentiment(user_input):
     vader_sentiment = vader_score(user_input)
-    vader_str = (f"**VADER Score :** {vader_sentiment}")
+    vader_str = (f"**VADER :**      {vader_sentiment}")
     st.write(vader_str)
+
+def transformersSentiment(user_input):
+    label, score = transformers_score(user_input)
+    transformers_str = f"**Transformers :** {label} ({score:.2f}%)"
+    st.write(transformers_str)
 
 def main():
     st.title("Sentiment Analysis App")
-    st.subheader("You typed:")
     # You can load an image from a file path or URL
     image_path = "Sentiment-Analysis.png"
     st.image(image_path, use_column_width=True)
     # Create a text input box for the user to input the string
-    user_input = st.text_area(label="Enter your string:", height= 200)
+    user_input = st.text_area(label="**Enter your Opinion:**", height= 200)
 
     # Sample data
     data = ["TextBlob", "Vader", "Transformers", "All"]
@@ -35,13 +39,12 @@ def main():
         VaderSentiment(user_input)
 
     elif selected_option=="Transformers":
-        st.write("Not added yet")
+        transformersSentiment(user_input)
     
     else:
         textblobSentiment(user_input)
         VaderSentiment(user_input)
-        Transformers_str = (f"**Transformer Score :** Not added yet")
-        st.write(Transformers_str)
+        transformersSentiment(user_input)
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from textblob import TextBlob
 # Valence Aware Dictionary for Sentiment Reasoning (VADER) is rule based sentiment analyzer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from transformers import pipeline
 
 def textblob_score(text):
     # create textBlob string
@@ -8,27 +9,41 @@ def textblob_score(text):
     # ranges -1 to 1 whether close to -1 is negative and close to 1 is posivite sentiment and neither close to -1 and 1 is neutral
     polarity = text.sentiment.polarity
     if polarity <= -0.05:
-        return f"negative sentiment ({polarity*100:.2f}%)"
+        return f"NEGATIVE SENTIMENT ({polarity*100:.2f}%)"
     elif polarity >-0.05 and polarity <0.05:
-        return f"neutral sentiment ({polarity*100:.2f}%)"
+        return f"NEUTRAL SENTIMENT ({polarity*100:.2f}%)"
     else:
-        return f"positive sentiment ({polarity*100:.2f}%)"
+        return f"POSITIVE SENTIMENT ({polarity*100:.2f}%)"
     
 
 
-# create sentiment Intensity Analyzer object
-vader_sia = SentimentIntensityAnalyzer()
-# calculate the polarity scores of sentiment text which provides the percentage of positivity, negativity, neutral & compound
-# compound range -1 to 1 and other ranges 0 to 1
+
 def vader_score(text):
+    # create sentiment Intensity Analyzer object
+    vader_sia = SentimentIntensityAnalyzer()
+    # calculate the polarity scores of sentiment text which provides the percentage of positivity, negativity, neutral & compound
+    # compound range -1 to 1 and other ranges 0 to 1
     # provides dictionary with pos, neg, neu & compound key and their values
     scores = vader_sia.polarity_scores(text)
     compound = scores['compound'] * 100
 
     # compound more than 5% is positive, from -5% to 5% neutral and less than 5% 
     if compound > 10:
-        return f"positive sentiment ({compound:.2f}%)"
+        return f"POSITIVE SENTIMENT ({compound:.2f}%)"
     elif compound <=10 and compound >=-10:
-        return f"neutral sentiment ({compound:.2f}%)"
+        return f"NEUTRAL SENTIMENT ({compound:.2f}%)"
     else:
-        return f"negative sentiment ({compound:.2f}%)"
+        return f"NEGATIVE SENTIMENT ({compound:.2f}%)"
+    
+
+def transformers_score(text):
+    # Initialize the sentiment analysis pipeline
+    # if model doesn't initialized, the given model will taken by default
+    sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")# Sample data
+
+    # Perform sentiment analysis
+    results = sentiment_pipeline(text)
+    label = results[0]['label']
+    score = results[0]['score']*100
+
+    return f"{label} SENTIMENT", score
